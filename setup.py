93e9b6267088a48ae7eeb8d95b373b3d5b4bd1b7
Rohith Desikan <rohithdesikan@gmail.com>
2020-06-24 23:03:54 -0700

    First Commit with a folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='evprediction',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project uses a 1500 home dataset to predict whether or not the house has an electric vehicle and when it is charging. ',
+    author='Rohith Desikan',
+    license='MIT',
+)
